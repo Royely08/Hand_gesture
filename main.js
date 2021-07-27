e227f6d9c5b5cbdd2ee1de897e5e4605583ea047
Lakshna S <78756909+Royely08@users.noreply.github.com>
2021-07-27 10:54:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -28,3 +28,43 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
 }
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_Hand_name1").innerHTML = results[0].label;
+        document.getElementById("result_Hand_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+
+        if(results[0].label == "peace"){
+            document.getElementById("update_handGesture1").innerHTML = "&#9996;";
+        }
+        if(results[0].label == "best"){
+            document.getElementById("update_handGesture1").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "amazing"){
+            document.getElementById("update_handGesture1").innerHTML = "&#128076;";
+        }
+        
+        if(results[1].label == "peace"){
+            document.getElementById("update_handGesture2").innerHTML = "&#9996;";
+        }
+        if(results[1].label == "best"){
+            document.getElementById("update_handGesture2").innerHTML = "&#128077;";
+        }
+        if(results[1].label == "amazing"){
+            document.getElementById("update_handGesture2").innerHTML = "&#128076;";
+        }
+        
+    }
+}
